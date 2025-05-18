7f8824fc3da2367c590ecc0f4d71ef0627ad81fb
keedongmon <rechardkee@gmail.com>
2025-05-18 14:51:32 +0900

    fix: WebSocket 엔드포인트에서 UserIdInterceptor 제외 처리

--- src/main/java/com/wowelang/chatserver/config/WebConfig.java
+++ src/main/java/com/wowelang/chatserver/config/WebConfig.java
@@ -17,7 +17,8 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(userIdInterceptor);
+        registry.addInterceptor(userIdInterceptor)
+                .excludePathPatterns("/ws/**");
     }
     
     @Override
